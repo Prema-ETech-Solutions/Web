473fa61117a26909a0880723917871a814b30f97
Prem Dangle <premdangle@gmail.com>
2022-08-07 02:14:06 +0530

    Home S1 left side completed

---
+++ Temp/Portfolio/JS/Animation.js
@@ -0,0 +1,9 @@
+// Auto typing Animation Start
+var typed = new Typed(".auto-typing", {
+    strings: ["HTML/CSS", "Javascript", "Python","Java", "C","Arduino","C++","VB"],
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 500,
+});
+// Auto typing Animation End
