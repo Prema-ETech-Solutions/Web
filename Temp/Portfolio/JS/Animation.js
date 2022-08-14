f9c8e87ba0408702a628c4fe98b4a7d3d5548bbc
Prem Dangle <premdangle@gmail.com>
2022-08-15 03:32:43 +0530

    Working on About Section

--- Temp/Portfolio/JS/Animation.js
+++ Temp/Portfolio/JS/Animation.js
@@ -1,12 +1,33 @@
+// Auto typing Animation Start
+var typed = new Typed(".auto-typing", {
+  strings: [
+    "HTML/CSS",
+    "Javascript",
+    "Python",
+    "Java",
+    "C",
+    "Arduino",
+    "C++",
+    "VB .NET",
+  ],
+  loop: true,
+  typeSpeed: 90,
+  backSpeed: 90,
+  backDelay: 1000,
+});
+// Auto typing Animation End
+
+
+
 //Home Load Function Start
 document.getElementById("s1").addEventListener("load", change_home_page());
 function change_home_page() {
   let x = Math.floor(Math.random() * 4 + 1);
   let y = Math.floor(Math.random() * 2 + 1);
-  console.log("Done");
+  // console.log("Done");
   // document.write("hhh");
-  console.log(x);
-  console.log(y);
+  // console.log(x);
+  // console.log(y);
   if (x == 1) {
     if (y == 1) {
       document.getElementById("s1").style.flexDirection = "row";
@@ -45,71 +66,59 @@ function change_home_page() {
 //Home Load Function end
 
 // About Load Function Start
-document.getElementById("about-title").addEventListener("load", change_about_page());
+document.getElementById("about").addEventListener("load", change_about_page());
 function change_about_page()
 {
-  console.log("OK");
-  document.getElementById("me").style.color = "#70ba65";
   let x = Math.floor(Math.random() * 4 + 1);
-  if(x == 1)
+  console.log(x);
+
+  if (x ==1)
   {
-    document.getElementById("about-logo-1").classList.remove("fa-regular");
-    document.getElementById("about-logo-1").classList.remove("fa-address-card");
-
-    document.getElementById("about-logo-1").classList.remove("fa-solid");
-    document.getElementById("about-logo-1").classList.remove("fa-user");
+    document.getElementById("Logo-about").classList.remove("fa-id-card-clip");
+    document.getElementById("Logo-about").classList.remove("fa-id-card");
+    document.getElementById("Logo-about").classList.remove("fa-address-card");
+    document.getElementById("Logo-about").classList.remove("fa-id-badge");
     
-    
-    document.getElementById("about-logo-1").classList.add("fa-solid");
-    document.getElementById("about-logo-1").classList.add("fa-address-card");
-    
+    document.getElementById("Logo-about").classList.add("fa-id-badge");
   }
-  else if(x == 2)
+  else if (x ==2)
   {
-    document.getElementById("about-logo-1").classList.remove("fa-regular");
-    document.getElementById("about-logo-1").classList.remove("fa-address-card");
-
-    document.getElementById("about-logo-1").classList.remove("fa-solid");
-    document.getElementById("about-logo-1").classList.remove("fa-address-card");
+    document.getElementById("Logo-about").classList.remove("fa-id-card-clip");
+    document.getElementById("Logo-about").classList.remove("fa-id-card");
+    document.getElementById("Logo-about").classList.remove("fa-address-card");
+    document.getElementById("Logo-about").classList.remove("fa-id-badge");
     
-    document.getElementById("about-logo-1").classList.add("fa-solid");
-    document.getElementById("about-logo-1").classList.add("fa-user");
+    document.getElementById("Logo-about").classList.add("fa-address-card");
   }
-  else
+  else if (x ==3)
   {
+    document.getElementById("Logo-about").classList.remove("fa-id-card-clip");
+    document.getElementById("Logo-about").classList.remove("fa-id-card");
+    document.getElementById("Logo-about").classList.remove("fa-address-card");
+    document.getElementById("Logo-about").classList.remove("fa-id-badge");
     
-    document.getElementById("about-logo-1").classList.remove("fa-solid");
-    document.getElementById("about-logo-1").classList.remove("fa-user");
-
-    document.getElementById("about-logo-1").classList.remove("fa-solid");
-    document.getElementById("about-logo-1").classList.remove("fa-address-card");
-
-    document.getElementById("about-logo-1").classList.add("fa-address-card");
-    document.getElementById("about-logo-1").classList.add("fa-regular fa-address-card");
+    document.getElementById("Logo-about").classList.add("fa-id-card");
   }
+  else if (x ==4)
+  {
+    document.getElementById("Logo-about").classList.remove("fa-id-card-clip");
+    document.getElementById("Logo-about").classList.remove("fa-id-card");
+    document.getElementById("Logo-about").classList.remove("fa-address-card");
+    document.getElementById("Logo-about").classList.remove("fa-id-badge");
+    
+    document.getElementById("Logo-about").classList.add("fa-id-card-clip");
+  }
+
 }
 // About Load Function End
 
+//About Values Starts 
+// document.getElementsByName("Ph").innerHTML = "+917039956119";
+document.getElementsByClassName("Ph").innerHTML = "+917039956119";
+
+//About Values Ends 
 
 
 
-// Auto typing Animation Start
-var typed = new Typed(".auto-typing", {
-  strings: [
-    "HTML/CSS",
-    "Javascript",
-    "Python",
-    "Java",
-    "C",
-    "Arduino",
-    "C++",
-    "VB .NET",
-  ],
-  loop: true,
-  typeSpeed: 90,
-  backSpeed: 90,
-  backDelay: 1000,
-});
-// Auto typing Animation End
 
 
